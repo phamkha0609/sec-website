b91d7e116a21fa14aba647a5b8ac8f48bb8a34fe
phuchoang85 <114634047+phuchoang85@users.noreply.github.com>
2025-03-07 07:11:02 +0700

    click access button move to coming-soon

--- app/coming-soon/page.tsx
+++ app/coming-soon/page.tsx
@@ -1,13 +1,20 @@
-import { Box } from '@mantine/core'
-import React from 'react'
-import Content from '../../features/ComingSoon/Content'
+import { Box } from "@mantine/core";
+import React from "react";
+import Content from "../../features/ComingSoon/Content";
+import Navbar from "../../features/Header/Navbar";
 
 function ComingSoon() {
   return (
-    <Box>
-        <Content />
+    <Box
+      style={{
+        overflowX: "hidden",
+      }}
+      pos={"relative"}
+    >
+      <Navbar />
+      <Content />
     </Box>
-  )
+  );
 }
 
-export default ComingSoon
+export default ComingSoon;
--- features/Header/CustomButton.tsx
+++ features/Header/CustomButton.tsx
@@ -1,14 +1,29 @@
-import { Button, Text } from '@mantine/core'
-import React from 'react'
+'use client';
+import { Button, Text } from "@mantine/core";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 function CustomButton() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/coming-soon");
+  };
   return (
-    <Button radius={100} px={{base: 24, xl: 36}} h={{base: 35, xl: 52}} bg={"linear-gradient(90deg, #272422, #060606)"} style={{
-      border: "2px solid #414141"
-    }}>
-        <Text fw={500} fz={{base: 12, xl: 18}}>Access App</Text>
+    <Button
+      radius={100}
+      px={{ base: 24, xl: 36 }}
+      h={{ base: 35, xl: 52 }}
+      bg={"linear-gradient(90deg, #272422, #060606)"}
+      style={{
+        border: "2px solid #414141",
+      }}
+      onClick={handleClick}
+    >
+      <Text fw={500} fz={{ base: 12, xl: 18 }}>
+        Access App
+      </Text>
     </Button>
-  )
+  );
 }
 
-export default CustomButton
+export default CustomButton;
